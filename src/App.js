6110cf3e7a588726d752ee5f0bd699fef24e979d
DESKTOP-F5MR2TE\USER <nahanju0402@naver.com>
2023-12-01 17:58:15 +0900

    react Blog 공부 1일차

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let titleList = [
+      {name: "남자 코트 추천", day:"2월 17일 발행", good: 0}, 
+      {name: "강남 우동 맛집", day:"2월 18일 발행", good: 0}, 
+      {name: "파이썬 독학", day:"2월 19일 발행", good: 0}
+    ];
+    let [titles, setTitles] = useState(titleList);
+    let [isMan, setIsMan] = useState(true);
+
+    function addGoodNum(index) {
+      let updateTitles = [...titles];
+      updateTitles[index].good += 1;
+      setTitles(updateTitles);
+    }
+
+    function coatChange() {
+      setIsMan(!isMan);
+      let updateTitles = [...titles];
+      if(isMan) {
+        updateTitles[0].name = "여자 코트 추천";
+      }
+      else {
+        updateTitles[0].name = "남자 코트 추천";
+      }
+      setTitles(updateTitles);
+    }
+
+    function orderByName() {
+      // name을 기준으로 정렬
+      const sortedTitles = [...titles].sort((a, b) => a.name.localeCompare(b.name));
+      setTitles(sortedTitles);
+    }
+
+    function orderByGood() {
+      const sortedTitles = [...titles].sort((a, b) => b.good - a.good);
+      setTitles(sortedTitles);
+    }
+    
+
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4
+                    style={{
+                        color: "yellow",
+                        fontSize: "16px",
+                    }}
+                >
+                    ReactBlog
+                </h4>
+            </div>
+            <div>
+              <button onClick={orderByName}>가나다순정렬</button>
+              <button onClick={orderByGood}>따봉순정렬</button>
+            </div>
+            {titles.map((title, index) => (
+                <div className="list" key={index}>
+                    <h4>
+                      {title.name} 
+                      <span onClick={() => addGoodNum(index)}>👍</span> {title.good}
+                    </h4>
+                    <p>{title.day}
+                      {title.name.includes("코트 추천") && <button onClick={coatChange}>글수정</button>}
+                    </p>
+                    
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
